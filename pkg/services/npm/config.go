9eaa881a7b72607d9911a5a57262df0d0cbabff1
Dave Sudia <dsudia@gospotcheck.com>
2020-08-19 10:50:02 -0600

    adds npm packaging code

---
+++ pkg/services/npm/config.go
@@ -0,0 +1,11 @@
+package npm
+
+type Config struct {
+	Publish         bool
+	PackageName     string
+	Email           string
+	ProjectURL      string
+	RegistryURL     string
+	ProtobufVersion string
+	Token           string
+}
