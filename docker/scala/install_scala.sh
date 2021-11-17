8788cae779fb19f9c4415e0182890cf4b381945a
Whitney Smestad <whitneysmestad@users.noreply.github.com>
2021-11-17 11:45:43 -0700

    Manually install sbt & let it handle scala versions

--- docker/scala/install_scala.sh
+++ docker/scala/install_scala.sh
@@ -1,22 +1,7 @@
 #! /usr/bin/env bash
 
 echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
+apk update && apk add openjdk8 gcc curl git wget ca-certificates
 
-apk update && apk add sbt openjdk8 gcc curl git wget ca-certificates
-
-SCALA_HOME=/usr/share/scala
-CROSS_SCALA_VERSIONS="${CURRENT_SCALA_VERSION} ${LEGACY_SCALA_VERSION}"
-
-for sparkVersion in $CROSS_SCALA_VERSIONS
-do
-  apk add --no-cache --virtual=.build-dependencies wget ca-certificates && \
-    apk add --no-cache bash && \
-    cd "/tmp" && \
-    wget "https://downloads.typesafe.com/scala/${sparkVersion}/scala-${sparkVersion}.tgz" && \
-    tar xzf "scala-${sparkVersion}.tgz" && \
-    mkdir "${SCALA_HOME}" && \
-    rm "/tmp/scala-${sparkVersion}/bin/"*.bat && \
-    mv "/tmp/scala-${sparkVersion}/bin" "/tmp/scala-${sparkVersion}/lib" "${SCALA_HOME}" && \
-    ln -s "${SCALA_HOME}/bin/"* "/usr/bin/" && \
-    rm -rf "/tmp/"*
-done
+# Manually installing sbt b/c apk fails with missing java-jdk constraint error
+wget -c "https://github.com/sbt/sbt/releases/download/v1.5.5/sbt-1.5.5.tgz" -O - | tar -xz -C /usr/share/
